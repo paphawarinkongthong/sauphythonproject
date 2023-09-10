30ae9e5426ef130de228fbe29575effe6c170d74
Taweekiat Jarumee <taweekiat@innova.co.th>
2023-09-10 21:48:31 +0700

    add test1.py

---
+++ lecture01/test1.py
@@ -0,0 +1,13 @@
+print('Hi...')
+print("Hi...")
+print('''
+      asdfasdf
+          asdfasdf
+          asdfasdfasdf
+              asdfasdf
+    ''')
+print(5465)
+print(4565.546)
+print(True)
+print(False)
+print(45 + 4 - 45) #expression นิพจน์
