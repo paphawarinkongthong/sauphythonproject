14c603017508d5456af52e5eceb3ee05b1f06a47
Paphawarin Kongthong <S6652410003@Sau.ac.th>
2023-09-11 19:43:58 +0700

    b

--- lecture02/test3.py
+++ lecture02/test3.py
@@ -1,4 +1,13 @@
 #รับค่าข้อมูลจากแป้นพิมพ์
-emp_name = input('ป้อนชื่อพนักงาน')
-sale_price = float(input('ป้อนยอดขาย :'))
-print("")
+emp_name = input("ป้อนชื่อพนักงาน : ")
+sale_price = float(input("ป้อนยอดขาย : "))
+print("------------")
+# แปลง str เป็น number -> int()จำนวนเต็ม flot()จำนวนจริงหรือทศนิยม
+commission = sale_price *10/100
+s = format(float(sale_price),".2f")
+c = format(float(commission),".2f")
+
+print(f'คุณ {emp_name} ยอดขาย {sale_price:.2f} บาท ได้ค่าคอม {commission:2f} บาท')
+print("คุณ", emp_name ,"ยอดขาย",s,"บาท ได้ค่าคอม",c,"บาท")
+print("คุณ"+ emp_name +"ยอดขาย"+str(s) +"บาท ได้ค่าคอม"+str(c) +"บาท")
+print("คุณ {} ยอดขาย {:.2f} บาท ได้ค่าคอม {:2f} บาท".format(emp_name,sale_price,commission))
