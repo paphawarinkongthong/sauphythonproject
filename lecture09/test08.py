2669b7184108a233c2e84f735d81039bc384b48a
paphawarinkongthong <142858959+paphawarinkongthong@users.noreply.github.com>
2023-11-29 13:34:41 +0700

    Add files via upload

---
+++ lecture09/test08.py
@@ -0,0 +1,8 @@
+import math
+def calSquareArea(wi,io):
+    return wi * io 
+def calTriangleArea(base,high):
+    return base* high / 2 
+
+def calCircleArea(radius):
+    return math.pi * math.pow(radius,2)
